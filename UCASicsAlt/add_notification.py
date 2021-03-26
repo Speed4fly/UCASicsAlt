ba923d888f1ffea87f3a2f30734543da5e582e3f
WJP <wangjiping@iie.ac.cn>
2021-03-26 16:10:32 +0800

    fix bugs & add -date option to start

--- UCASicsAlt/add_notification.py
+++ UCASicsAlt/add_notification.py
@@ -1,12 +1,17 @@
 import click
 import re
+import time
 
 
 @click.command()
 @click.option('-t', '--time', default=30, help='提前发出通知的时间分钟数')
+@click.option('-d', '--date',
+              default=int(time.localtime().tm_year * 10000 + time.localtime().tm_mon * 100 + time.localtime().tm_mday),
+              help='导入开始的日期,默认为今天,格式为yyyymmdd.')
 @click.option('-s', '--source', default='schedule.ics', help='要修改的ics文件路径')
-def cli(time, source):
+def cli(time, date, source):
     time_reg = re.compile(r'\d{8}T\d{6}')
+    # print(date)
     record = []
     records = []
     target = open('./result.ics', 'w', encoding='utf-8')
@@ -16,27 +21,36 @@ def cli(time, source):
         for index in range(len(lines)):
             if lines[index] == 'BEGIN:VEVENT\n':
                 record.append(index)
+                if not flag:
+                    init = index
                 flag = True
             if lines[index][:7] == 'SUMMARY':
-                record.append(lines[index])
+                record.append(lines[index][7:-1])
             if lines[index][:7] == 'DTSTART' and flag:
                 # record.append(lines[index])
                 tmp = re.findall(time_reg, lines[index])
                 record.append([int(tmp[0][:8]), int(tmp[0][9:])])
             if lines[index] == 'END:VEVENT\n':
                 record.append(index)
-                records.append(record)
+                if record[1][0] >= date:
+                    records.append(record)
+                    # print(record)
+                    # print(record[1][0])
                 record = []
-        last_class_name = records[0][1]
+        if len(records) == 0:
+            print('No new course.')
+            return 0
+        last_class_name = records[0][2]
         last_class_index = 0
-        cnt = 1
-        for index_lines in range(records[0][0]):
+        last_class_date = records[0][1][0]
+        last_class_time = records[0][1][1]
+        cnt = 0
+        for index_lines in range(init):
             target.write(lines[index_lines])
         for index in range(len(records)):
             # print(records[index])
-            if ((records[index][2] == last_class_name and records[index][1][0] == records[index - 1][1][0]) and
-                    ((records[index][1][1] - records[index - 1][1][1]) < 11001)):
-
+            if ((records[index][2] == last_class_name and records[index][1][0] == last_class_date) and
+                    ((records[index][1][1] - last_class_time) < 11001)):
                 cnt += 1
             else:
                 if cnt < 3:
@@ -54,4 +68,6 @@ def cli(time, source):
                 cnt = 1
                 last_class_name = records[index][2]
                 last_class_index = index
+            last_class_date = records[index][1][0]
+            last_class_time = records[index][1][1]
     target.close()
